a574629162a04f3ebf6abce9a9cd8507d35c93b1
Chris <chrsschamschula@gmail.com>
2024-09-17 12:35:09 -0700

    Set up Eleventy to output to docs for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = function(eleventyConfig) {
-  // Pass through copy for CSS or other assets if needed
+  // Pass through copy for CSS and images if needed
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images"); // If you have images
 
   return {
     dir: {
       input: "src",
-      output: "docs" // Change this from 'dist' to 'docs'
+      output: "docs" // Set output to 'docs' for GitHub Pages
     }
   };
 };
